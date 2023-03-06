4546c7657bad0308db05f257a88da9958ecb8caa
lichuang <278747148@qq.com>
2023-03-06 23:59:24 +0800

    新增 base-rule 组件

---
+++ base-rule/src/main/java/com/lichuang/App.java
@@ -0,0 +1,13 @@
+package com.lichuang;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
